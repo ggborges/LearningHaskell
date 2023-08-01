973b1b0e9369d6204afed9e35a6612f435679639
Gustavo Borges <ggb@cin.ufpe.br>
2023-08-01 01:54:12 -0300

    att

--- Estudos/video_monitoria.hs
+++ Estudos/video_monitoria.hs
@@ -9,14 +9,27 @@ merge (x:xs) (y:ys)
     | x <= y = x : merge xs (y:ys)
     | otherwise = y : merge (x:xs) ys
 
-metade :: (Num a) => [a] -> ([a],[a])
+-- ---------------------------
+
+metade :: [a] -> ([a],[a])
 metade [] = ([],[])
-metade xs = (allPar xs, allImpar xs)
+metade xs = ((allPar xs 0), (allImpar xs 0))
 
-allPar :: (Num a, Eq a) => [a] -> [a]
-allPar [] = []
-allPar xs = [x | x <- xs, (x `mod` 2) == 0]
+allPar :: [a] -> Int -> [a]
+allPar [] _ = []
+allPar (x:xs) i
+    | mod i 2 == 0 = x : allPar xs (i+1)
+    | otherwise    = allPar xs (i+1)
 
-allImpar :: (Num a, Eq a) => [a] -> [a]
-allImpar [] = []
-allImpar xs = [x | x <- xs, not ((x `mod` 2) == 0)]
+allImpar :: [a] -> Int -> [a]
+allImpar [] _ = []
+allImpar (x:xs) i
+    | mod i 2 /= 0 = x : allImpar xs (i+1)
+    | otherwise    = allImpar xs (i+1)
+
+
+mSort :: Ord a => [a] -> [a]
+mSort []  = []
+mSort [x] = [x]
+mSort xs  = let (metade1, metade2) = metade xs
+            in merge (mSort (metade1)) (mSort metade2)
